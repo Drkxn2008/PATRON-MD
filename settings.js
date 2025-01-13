50ecae386b80e01ba107ca81bf950df2f7f81caa
Drkxn2008 <drkxnthegod@gmail.com>
2025-01-13 09:26:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "DRKXN"
 
-global.banknumber = "8133729715"
+global.banknumber = "5272752527635262"
 
-global.bankowner = "FADARE"
+global.bankowner = "DRKXN"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
